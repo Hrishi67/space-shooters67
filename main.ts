f649f68cb07d61a37c7702f2d5e2136cd395c129
Hrishi67 <vitalqb3@gmail.com>
2025-11-03 12:28:11 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+let mainPlayer = sprites.create(img`
+    .cccccccccccccc.
+    cb777777777777bc
+    c77777777777777c
+    c77777777777777c
+    c77777777777777c
+    c77777777777777c
+    c77777777777777c
+    c77777777777777c
+    c76666666666667c
+    c66666666666666c
+    c67777777777776c
+    c6c666c66c666c6c
+    f6c666cddc666c6f
+    f6c6666cc6666c6f
+    f6cccccccccccc6f
+    f66666666666666f
+    f67777777777776f
+    f6c666c66c666c6f
+    f6c666cddc666c6f
+    f6c6666cc6666c6f
+    f6cccccccccccc6f
+    f66666666666666f
+    f6ffffffffffff6f
+    ffffffffffffffff
+    `, SpriteKind.Player)
+mainPlayer.setScale(0.5, ScaleAnchor.Middle)
